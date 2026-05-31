84120b6dca1f5f29aef6a2c4dd2c97b2e784ff4c
d1y <chenhonzhou@gmail.com>
2026-05-31 17:48:19 +0800

    chore: adjust log view text sizing

--- Cyanide/LogTextView.m
+++ Cyanide/LogTextView.m
@@ -358,7 +358,7 @@ static UIColor *colorForLogLine(NSString *line) {
 
 - (void)setup {
     self.editable   = NO;
-    self.font       = [UIFont monospacedSystemFontOfSize:13 weight:UIFontWeightRegular];
+    self.font       = [UIFont monospacedSystemFontOfSize:12 weight:UIFontWeightRegular];
     self.backgroundColor = [UIColor colorWithRed:0.02 green:0.05 blue:0.06 alpha:1.0];
     self.textColor  = UIColor.whiteColor;
     self.textContainerInset = UIEdgeInsetsMake(12, 10, 12, 10);
@@ -384,9 +384,9 @@ static UIColor *colorForLogLine(NSString *line) {
 }
 
 - (NSMutableAttributedString *)buildAttrStringForText:(NSString *)text {
-    UIFont *font = self.font ?: [UIFont monospacedSystemFontOfSize:13 weight:UIFontWeightRegular];
+    UIFont *font = self.font ?: [UIFont monospacedSystemFontOfSize:12 weight:UIFontWeightRegular];
     NSMutableParagraphStyle *para = [[NSMutableParagraphStyle alloc] init];
-    para.lineSpacing = 4.0;
+    para.lineSpacing = 3.0;
 
     NSArray<NSString *> *lines = [text componentsSeparatedByString:@"\n"];
     NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] init];
